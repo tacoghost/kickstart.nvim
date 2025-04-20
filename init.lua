581e06cfb0daf244a51c69bae49f17a327a4e7ca
tacoghost <207381904+tacoghost@users.noreply.github.com>
2025-04-20 11:06:26 -0700

    add formatting support for js, json, ts, and vue files with prettier

--- init.lua
+++ init.lua
@@ -781,7 +781,10 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        vue = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
