487f3f5dbe4355437c64afe9eb6b334aaf9f7a22
tacoghost <207381904+tacoghost@users.noreply.github.com>
2025-04-23 15:29:21 -0700

    more csharp configurations

--- lua/custom/plugins/roslyn.lua
+++ lua/custom/plugins/roslyn.lua
@@ -6,6 +6,10 @@ return {
     --@type RoslynNvimConfig
     opts = {
       settings = {
+        ['csharp|background_analysis'] = {
+          dotnet_analyzer_diagnostics_scope = 'fullSolution',
+          dotnet_compiler_diagnostics_scope = 'fullSolution',
+        },
         ['csharp|inlay_hints'] = {
           csharp_enable_inlay_hints_for_implicit_object_creation = true,
           csharp_enable_inlay_hints_for_implicit_variable_types = true,
@@ -22,6 +26,18 @@ return {
         },
         ['csharp|code_lens'] = {
           dotnet_enable_references_code_lens = true,
+          dotnet_enable_tests_code = true,
+        },
+        ['csharp|completion'] = {
+          dotnet_provide_regex_completions = true,
+          dotnet_show_completion_items_from_unimported_namespaces = true,
+          dotnet_show_name_completion_suggestions = true,
+        },
+        ['csharp|symbol_search'] = {
+          dotnet_search_reference_assemblies = true,
+        },
+        ['csharp|formatting'] = {
+          dotnet_organize_imports_on_format = true,
         },
       },
     },
